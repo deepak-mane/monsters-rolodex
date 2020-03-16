29f5c4761927f22e7fc611877b1b3229fd6e83cb
Deepak Mane <deepak.mane123@gmail.com>
2020-03-16 00:57:34 -0700

    Updates

---
+++ precache-manifest.7e82abac1c9833e6c536facfabcc6c9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b859187c24a94828e2878ede6e80d6a1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f63bf0ac499014e79b67",
+    "url": "/monsters-rolodex/static/css/main.1fdd3e09.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f63bf0ac499014e79b67",
+    "url": "/monsters-rolodex/static/js/main.c596bad1.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
